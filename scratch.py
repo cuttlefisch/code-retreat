59af895943b2eec7f50b3f53648ada267c879ca1
cuttlefisch <hayden@collaborative.li>
2021-03-04 13:17:54 -0800

    initial scratch file

---
+++ scratch.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import pytest
+
+# Initial tests for cell lifecyle
+
+# A) If cell has fewer than two neighbors then die
+
+# B) If cel has two or three neighbors then live
+
+# C) if cel has greater than three neighbors then die
+
+# D) if cell has exactly three neighbors, and is dead, then revive (set to live)
